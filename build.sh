d83140cdc57bb62eb41b59f80b925e118a158339
Mark Westerhof <mwesterhof@fortinet.com>
2017-12-01 23:47:18 -0800

    Build themes

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 
 # Uses https://github.com/golf1052/base16-builder-typescript
 
+DEST='autoload/lightline/colorscheme'
+
+rm -rf sources
 base16-builder update
 ln -s $(pwd) sources/templates/vim-lightline-base16
 base16-builder build
-rm autoload/lightline/colorscheme/*
-cp themes/vim-lightline-base16/autoload/lightline/colorscheme/* autoload/lightline/colorscheme
+rm $DEST/*
+cp themes/vim-lightline-base16/$DEST/* $DEST
+
+# Can't have dashes in file names or variables.
+find $DEST -type f -exec sed -i 's/-/_/g' {} \;
+for file in $DEST/*; do mv $file "${file//-/_}"; done
